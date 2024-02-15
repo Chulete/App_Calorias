b9d495e3b21621ddcb0bf39e36d2e5ff7329dc6b
Chule_TE <137904172+Chulete@users.noreply.github.com>
2024-02-15 20:55:46 +0100

    Update 2_Contar_calorias.py

--- pages/2_Contar_calorias.py
+++ pages/2_Contar_calorias.py
@@ -5,13 +5,6 @@ import streamlit.components.v1 as stc
 import pandas as pd 
 import base64
 
-
-
-df_fruta = pd.read_excel("https://docs.google.com/spreadsheets/d/1kIXM95WfLygUXIHcpBBFkGMHnrDhrnTU/edit#gid=729249405")
-df_verdura = pd.read_excel("https://github.com/Chulete/App_Calorias/blob/master/calorias_alimentos/calorias_verdura.xlsx")
-df_carne = pd.read_excel("https://github.com/Chulete/App_Calorias/blob/master/calorias_alimentos/calorias_carne.xlsx")
-df_pescado = pd.read_excel("https://github.com/Chulete/App_Calorias/blob/master/calorias_alimentos/calorias_pescado.xlsx")
-df_pasta = pd.read_excel("https://github.com/Chulete/App_Calorias/blob/master/calorias_alimentos/calorias_pasta.xlsx")
 st.subheader("Cómo las cuento")
 stc.iframe("https://i.pinimg.com/originals/c5/a2/4d/c5a24d98dcd5eae25128f7b04f04223a.gif",
 	height=420, width=800, scrolling=True)
@@ -28,22 +21,27 @@ if peso_alimento is not None and calorias is not None:
 	total_calorias = (peso_alimento * calorias) / 100
 	st.write(f"{total_calorias} calorías")
 
-
-col1, col2 = st.columns([2,2])
-with col1:
-	with st.expander("Fruta"):
-		st.dataframe(df_fruta)
-with col2:
-	with st.expander("Verdura"):
-		st.dataframe(df_verdura)
-
 col3, col4 = st.columns([2,2])
 with col3:
-	with st.expander("Carne"):
-		st.dataframe(df_carne)
+	peso_alimento_2 = st.number_input("Peso del alimento")
 with col4:
-	with st.expander("Pescado"):
-		st.dataframe(df_pescado)
+	calorias_2 = st.number_input("Calorías por cada 100 gramos")
+if peso_alimento_2 is not None and calorias is not None:
+	total_calorias_2 = (peso_alimento_2 * calorias_2) / 100
+	st.write(f"{total_calorias_2} calorías")
+
+col5, col6 = st.columns([2,2])
+with col5:
+	peso_alimento_3 = st.number_input("Peso del alimento")
+with col6:
+	calorias_3 = st.number_input("Calorías por cada 100 gramos")
+if peso_alimento_3 is not None and calorias is not None:
+	total_calorias_3 = (peso_alimento_3 * calorias_3) / 100
+	st.write(f"{total_calorias_3} calorías")
+
+suma_total_calorias = total_calorias + total_calorias_2 + total_calorias_3
+st.write(f'Total calorías añadidas {suma_total_calorias}')
+
 
 
 
