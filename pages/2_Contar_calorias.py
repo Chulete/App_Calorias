b056d8968d88c6d7c645511e695e79ed2e0bf859
Chule_TE <137904172+Chulete@users.noreply.github.com>
2024-02-15 20:59:41 +0100

    Update 2_Contar_calorias.py

--- pages/2_Contar_calorias.py
+++ pages/2_Contar_calorias.py
@@ -21,26 +21,6 @@ if peso_alimento is not None and calorias is not None:
 	total_calorias = (peso_alimento * calorias) / 100
 	st.write(f"{total_calorias} calorías")
 
-col3, col4 = st.columns([2,2])
-with col3:
-	peso_alimento_2 = st.number_input("Peso del alimento")
-with col4:
-	calorias_2 = st.number_input("Calorías por cada 100 gramos")
-if peso_alimento_2 is not None and calorias_2 is not None:
-	total_calorias_2 = (peso_alimento_2 * calorias_2) / 100
-	st.write(f"{total_calorias_2} calorías")
-
-col5, col6 = st.columns([2,2])
-with col5:
-	peso_alimento_3 = st.number_input("Peso del alimento")
-with col6:
-	calorias_3 = st.number_input("Calorías por cada 100 gramos")
-if peso_alimento_3 is not None and calorias_3 is not None:
-	total_calorias_3 = (peso_alimento_3 * calorias_3) / 100
-	st.write(f"{total_calorias_3} calorías")
-
-suma_total_calorias = total_calorias + total_calorias_2 + total_calorias_3
-st.write(f'Total calorías añadidas {suma_total_calorias}')
 
 
 
