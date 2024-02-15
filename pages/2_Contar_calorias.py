c04286a678930949900d515350ae0fb2eadc6593
Chule_TE <137904172+Chulete@users.noreply.github.com>
2024-02-15 21:07:30 +0100

    Update 2_Contar_calorias.py

--- pages/2_Contar_calorias.py
+++ pages/2_Contar_calorias.py
@@ -11,29 +11,13 @@ stc.iframe("https://i.pinimg.com/originals/c5/a2/4d/c5a24d98dcd5eae25128f7b04f04
 st.write("""Esto es tan fácil como coger una tana, váscula... y pesar la comida.
 	En los siguientes deplegables se muestra las calorías por cada 100 gramos de alimento ...
 	""")
-# Cálculo del primer alimento
-with st.expander("Alimento 1"):
-    peso_alimento_1 = st.number_input("Peso del alimento")
-    calorias_1 = st.number_input("Calorías por cada 100 gramos")
-    total_calorias_1 = (peso_alimento_1 * calorias_1) / 100
-    st.write(f"{total_calorias_1} calorías")
-
-# Cálculo del segundo alimento
-with st.expander("Alimento 2"):
-    peso_alimento_2 = st.number_input("Peso del alimento")
-    calorias_2 = st.number_input("Calorías por cada 100 gramos")
-    total_calorias_2 = (peso_alimento_2 * calorias_2) / 100
-    st.write(f"{total_calorias_2} calorías")
-
-# Cálculo del tercer alimento
-with st.expander("Alimento 3"):
-    peso_alimento_3 = st.number_input("Peso del alimento")
-    calorias_3 = st.number_input("Calorías por cada 100 gramos")
-    total_calorias_3 = (peso_alimento_3 * calorias_3) / 100
-    st.write(f"{total_calorias_3} calorías")
-
-suma_total_calorias = total_calorias_1 + total_calorias_2 + total_calorias_3
-st.write(f'Total calorías añadidas {suma_total_calorias}')
+col1, col2 = st.columns([2,2])
+with col1:
+	peso_alimento = st.number_input("Peso del alimento")
+with col2:
+	calorias = st.number_input("Calorías por cada 100 gramos")
+	total_calorias = (peso_alimento * calorias) / 100
+	st.write(f"{total_calorias} calorías")
 
 
 
